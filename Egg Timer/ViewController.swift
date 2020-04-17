27cf3860b6bf61460ad9af47abc375e857ddaf1b
Rollin Francois <rfrancois21@hotmail.com>
2020-04-16 21:58:11 -0400

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Rollin Francois on 4/16/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
